134ef9f932667810654506dfe00550571306cc88
Dante Santos <dante.alighierimds@gmail.com>
2017-11-17 20:02:29 -0300

    Update README

--- image_magnitude.c
+++ image_magnitude.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "structs.h"
+
 // Verificar as formas circulares do olho e escrever o arquivo de resultado
 // TODO: Aplicar a transformação de Houg, verificar valores e criar o arquivo de resultado
 void HoughTransformation(int heigth, int width, int *pixels, FILE *file) {
